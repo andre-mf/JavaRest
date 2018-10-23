e6dffd45b9946c421d140369fd9ba1376636850a
andre-mf <andremendesfelix@gmail.com>
2018-10-22 22:51:29 -0200

    Arquivos JavaRest

---
+++ src/java/br/iesb/poo2/rest/Mensagens.java
@@ -0,0 +1,44 @@
+package br.iesb.poo2.rest;
+
+import java.util.Date;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("msg")
+public class Mensagens {
+    
+    Date tempo = new Date();
+    @Path("teste01")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String doTeste01() {
+        return "Resposta em txt RESTFUll, O tempo agora é "+ tempo;
+    }
+    
+    @Path("teste02")
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public String doTeste02() {
+        return "<?xml version=\"1.0\"?>" + "<hello> Formato xml" +tempo+ "</hello>";       
+    }
+    
+    @Path("teste03")
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String doTeste03() {
+        return "<html> " + "<title>" + "Formato HTML" + "</title>"  
+        + "<body><h1>" + "Mensagem em  HTML" + "</h1></body>" +tempo+"</html> ";       
+    }
+    
+    @Path("teste04")
+    @GET
+    @Consumes({MediaType.APPLICATION_JSON})
+    @Produces({MediaType.TEXT_PLAIN})
+    public String doTeste04() {
+        return "TEMPO=" + tempo;       
+    }
+}
---
+++ src/java/br/iesb/poo2/rest/RestApplication.java
@@ -0,0 +1,10 @@
+package br.iesb.poo2.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+
+public class RestApplication extends Application{
+    
+}
