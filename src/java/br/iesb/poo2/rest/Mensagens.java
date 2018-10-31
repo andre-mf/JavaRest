4518618e1018527ccd26614a82c2be97ad21a509
andre-mf <andremendesfelix@gmail.com>
2018-10-31 12:09:04 -0300

    Inclusão do simulador

--- src/java/br/iesb/poo2/rest/Mensagens.java
+++ src/java/br/iesb/poo2/rest/Mensagens.java
@@ -1,6 +1,8 @@
 package br.iesb.poo2.rest;
 
+import java.text.NumberFormat;
 import java.util.Date;
+import java.util.Locale;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -38,8 +40,20 @@ public class Mensagens {
     @Path("teste04")
     @GET
     @Consumes({MediaType.APPLICATION_JSON})
-    @Produces({MediaType.TEXT_PLAIN})
-    public String doTeste04() {
-        return "TEMPO=" + tempo;       
+    @Produces({MediaType.APPLICATION_JSON})
+    public String doTeste04(@QueryParam("parcelas") int parcelas) {
+        return "o numero é " + parcelas;
+    }
+    
+    @Path("teste05")
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String doTeste05(@QueryParam("parcelas") int parcelas, @QueryParam("valor") double valor) {
+        double total = ((valor * 0.10) * parcelas) + valor;
+        double vlrParcelas = total / parcelas;
+        double juros = total - valor;
+        Locale localeBR = new Locale("pt","BR");
+        NumberFormat dinheiro = NumberFormat.getCurrencyInstance(localeBR);
+        return "Seu empréstimo de <strong>R$ " + dinheiro.format(valor) +"</strong>,<br>parcelado em <strong>" + parcelas + " parcelas de " + dinheiro.format(vlrParcelas) + "</strong>,<br>custará <strong> " + dinheiro.format(total) + "</strong><br><br>(valor total de juros: <strong>" + dinheiro.format(juros) + "</strong>).";
     }
 }
